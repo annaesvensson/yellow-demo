775b9588fbfa80b3c5289b14311b15b2a1086dbd
markseu <mark2011@mayberg.se>
2022-10-28 19:33:21 +0200

    Added previously unpublished extension
    Co-Authored-By: Anna <14218799+annaesvensson@users.noreply.github.com>

---
+++ demo.php
@@ -0,0 +1,56 @@
+<?php
+// Demo extension, https://github.com/annaesvensson/yellow-demo
+
+class YellowDemo {
+    const VERSION = "0.8.7";
+    public $yellow;         // access to API
+
+    // Handle initialisation
+    public function onLoad($yellow) {
+        $this->yellow = $yellow;
+    }
+    
+    // Handle page meta data
+    public function onParseMetaData($page) {
+        if ($page===$this->yellow->page) {
+            $prefix = strtoloweru($this->yellow->language->getText("languageDescription", $page->get("language")));
+            $page->set("editLoginEmail", "$prefix@demo.com");
+            $page->set("editLoginPassword", "password");
+        }
+    }
+    
+    // Handle update
+    public function onUpdate($action) {
+        $fileNameUser = $this->yellow->system->get("coreExtensionDirectory").$this->yellow->system->get("coreUserFile");
+        if ($action=="install") {
+            foreach ($this->yellow->system->getAvailable("language") as $language) {
+                $prefix = strtoloweru($this->yellow->language->getText("languageDescription", $language));
+                $email = "$prefix@demo.com";
+                if (!$this->yellow->user->isExisting($email)) {
+                    $settings = array(
+                        "name" => "Demo",
+                        "description" => $this->yellow->language->getText("editUserDescription", $language),
+                        "language" => $language,
+                        "access" => "edit, upload",
+                        "home" => "/demo/",
+                        "hash" => $this->yellow->extension->get("edit")->response->createHash("password"),
+                        "stamp" => $this->yellow->extension->get("edit")->response->createStamp(),
+                        "pending" => "none",
+                        "failed" => "0",
+                        "modified" => date("Y-m-d H:i:s", time()),
+                        "status" => "active");
+                    $ok = $this->yellow->user->save($fileNameUser, $email, $settings);
+                    $this->yellow->log($ok ? "info" : "error", "Add user 'Demo'");
+                }
+            }
+        } elseif ($action=="uninstall") {
+            foreach ($this->yellow->system->getAvailable("email") as $email) {
+                if (!preg_match("/@demo.com$/", $email)) continue;
+                if (!preg_match("/demo/", $this->yellow->user->getUser("home", $email))) continue;
+                $name = $this->yellow->user->getUser("name", $email);
+                $ok = $this->yellow->user->remove($fileNameUser, $email);
+                $this->yellow->log($ok ? "info" : "error", "Remove user '".strtok($name, " ")."'");
+            }
+        }
+    }
+}
